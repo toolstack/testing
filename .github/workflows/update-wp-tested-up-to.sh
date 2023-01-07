895a188ec5edd93b0dfd4eeb4233a0a713099566
Greg <greg@toolstack.com>
2023-01-07 17:38:32 -0500

    update

--- .github/workflows/update-wp-tested-up-to.sh
+++ .github/workflows/update-wp-tested-up-to.sh
@@ -2,6 +2,7 @@
 
 echo "Retrieving current WordPress version from wordpress.org..."
 wget http://api.wordpress.org/core/stable-check/1.0/ -O stable.check.txt
+
 tail -n 2 stable.check.txt > stable.tail.txt
 head -n 1 stable.tail.txt > stable.txt
 sed -i 's/ : "latest"//' stable.txt
@@ -11,4 +12,12 @@ WP_VERSION=$(<stable.txt)
 
 rm stable*.txt
 
-echo "Current WordPress version is: $WP_VERSION"
+if [ -z "$WP_VERSION" ]; then
+	echo "ERROR: Could not retrieve current WordPress version from wordpress.org."
+else
+	echo "Current WordPress version is: $WP_VERSION"
+
+	sed -i "s/^Tested up to: .*/Tested up to: $WP_VERSION/" ${GITHUB_WORKSPACE}/readme.txt
+
+	cat ${GITHUB_WORKSPACE}/readme.txt
+fi
